61eb1c2f1f2d80f0c8b40b59c3d47f6474c0d461
Sushmithaselvam <sushmithacse@ksrce.ac.in>
2016-08-13 13:40:17 +0530

    Create Greatest .java

---
+++ Greatest .java
@@ -0,0 +1,25 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class Greatest {
+public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("enter any three numbers");
+	int number1=scanner.nextInt();
+	int number2=scanner.nextInt();
+	int number3=scanner.nextInt();
+	
+		if(number1>number2&&number1>number3){
+			System.out.println(number1+" is greater");
+		}
+		else if(number2>number3){
+			System.out.println(number2+" is greater");
+		}
+		else{
+			System.out.println(number3+" is greater");
+		}
+	
+	scanner.close();
+}
+}
