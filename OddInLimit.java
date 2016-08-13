4ac2b3e6a92d792e63dd939c1a0ef34f33ca6227
Sushmithaselvam <sushmithacse@ksrce.ac.in>
2016-08-13 14:03:09 +0530

    Create OddInLimit.java

---
+++ OddInLimit.java
@@ -0,0 +1,19 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class OddInLimit {
+public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("enter the lowerlimit");
+	int lowerlimit=scanner.nextInt();
+	System.out.println("enter the upperlimit");
+	int upperlimit=scanner.nextInt();
+	for(int i=lowerlimit;i<=upperlimit;i++){
+		if(i%2!=0){
+			System.out.println(i+" is an odd number");
+		}
+	}
+	scanner.close();
+}
+}
